910652e03de7258ae63f8144a14cd4a4fa83a0e6
SapphireGaze <jasminehuang2048@outlook.com>
2024-04-13 11:23:16 -0700

    Finished Base Backend:  - Finished quest and waypoint models  - Finished create and play routers  - Added types.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, { NextFunction } from "express";
 import mongoose from "mongoose";
 
+import CreateRouter from "./routers/create.router";
+import PlayRouter from "./routers/play.router";
+
 require("dotenv").config();
 const port: number = Number(process.env.PORT) || 8000;
 
@@ -16,6 +19,9 @@ app.use((req: express.Request, res: express.Response, next: NextFunction) => {
   ]);
   next();
 });
+
+app.use("/v1/api/create", CreateRouter);
+app.use("/v1/api/play", PlayRouter);
 
 mongoose
   .connect(process.env.DB_HOST as string)
