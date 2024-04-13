458f0790d7ae2f507a7d83754889c778962c638a
SapphireGaze <jasminehuang2048@outlook.com>
2024-04-13 13:49:55 -0700

    Landing Page:  - Initial landing page

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import Logo from './assets/hero.jpg';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.h1}>QuestScape</Text>
+      </View>
+      <View style={styles.middleContainer}>
+        <Image
+          source={Logo}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.createButtonContainer}>
+          <Button
+            title="Create A Game"
+            style={styles.createButtonContainer}
+            onPress={() => this.onPress()}
+            color="#fff"
+          />
+        </View>
+        <View style={styles.joinButtonContainer}>
+          <Button
+            title="Join A Game"
+            style={styles.joinButtonContainer}
+            onPress={() => this.onPress()}
+            color="#fff"
+          />
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#deb887',
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-evenly',
+  },
+  topContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
+    padding: 10,
   },
+  middleContainer: {
+    flex: 3,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  bottomContainer: {
+    justifyContent: 'space-between',
+    width: '90%',
+    margin: 20,
+    padding: 10,
+
+  },
+  h1: {
+    color: '#87ADDE',
+    fontSize: 48,
+  },
+  buttonContainer: {
+    borderRadius: 10,
+    padding: 8,
+    margin: 8,
+  },
+  createButtonContainer: {
+    marginTop: 'auto',
+    backgroundColor: '#8b2861',
+    borderRadius: 10,
+    padding: 8,
+    margin: 8,
+  },
+  joinButtonContainer: {
+    marginTop: 'auto',
+    backgroundColor: '#fe9a16',
+    borderRadius: 10,
+    padding: 8,
+    margin: 8,
+  },
+  image: {
+    width: 300,
+    height: 260,
+    justifyContent: 'center',
+  }
 });
