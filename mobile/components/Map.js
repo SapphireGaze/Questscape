3114578d054fa033e7019477b951e4ebe52d1fc6
SapphireGaze <jasminehuang2048@outlook.com>
2024-04-14 21:08:55 -0700

    Reworked Map Component to Functional Component:  - Reworked map to be functional component instead of class component  - Made marker draggable

--- mobile/components/Map.js
+++ mobile/components/Map.js
@@ -1,29 +1,35 @@
-import React, { Component } from "react";
-import { Dimensions, View, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 import pin from "../assets/custom_pin.png";
 
-const { height, width } = Dimensions.get("window");
+export default function Map() {
+  const [draggableCoord, setDraggableCoord] = useState({
+    latitude: 39.541199,
+    longitude: -119.806107,
+  });
 
-export default class Map extends Component {
-  render() {
-    return (
-      <View style={{ height: height, width: width }}>
-        <MapView
-          style={{ position: "absolute", left: 0, top: 0, right: 0, bottom: 0 }}
-          initialRegion={{
-            latitude: 39.541199,
-            longitude: -119.806107,
-            latitudeDelta: 0.042,
-            longitudeDelta: 0.042,
-          }}
+  return (
+    <View className="h-full w-full">
+      <MapView
+        className="absolute bottom-0 left-0 right-0 top-0"
+        initialRegion={{
+          latitude: 39.541199,
+          longitude: -119.806107,
+          latitudeDelta: 0.042,
+          longitudeDelta: 0.042,
+        }}
+      >
+        <Marker
+          draggable
+          identifier="1"
+          coordinate={draggableCoord}
+          onDragEnd={(e) => e.nativeEvent.coordinate}
         >
-          <Marker coordinate={{ latitude: 39.541199, longitude: -119.806107 }}>
-            <Image source={pin} style={{ width: 30, height: 30 }} />
-          </Marker>
-        </MapView>
-      </View>
-    );
-  }
+          <Image source={pin} style={{ width: 40, height: 40 }} />
+        </Marker>
+      </MapView>
+    </View>
+  );
 }
